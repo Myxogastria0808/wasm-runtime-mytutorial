651d6e217e28a8c5f15777c7021b19e0316d82a2
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-09-25 15:49:13 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "rust flake sample";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              # for axum
+              openssl
+              # background code checker
+              pkg-config
+              bacon
+              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+            ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+      }
+    );
+}
